9317bf73ae1181d7bc60cb159df0582ed1453af5
Tomas <69489194+NitsugaSamot@users.noreply.github.com>
2023-10-02 00:30:24 -0300

    modificando envio de email

--- helpers/emailOlvidePassword.js
+++ helpers/emailOlvidePassword.js
@@ -1,14 +1,18 @@
 import nodemailer from 'nodemailer'
 
 const emailOlvidePassword = async (datos) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT ,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        },
-      });
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
     //   console.log(datos)
 
     const {email, nombre, token} = datos
--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -2,13 +2,17 @@ import nodemailer from 'nodemailer'
 
 const emailRegistro = async (datos) => {
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT ,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        },
-      });
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
     //   console.log(datos)
 
     const {email, nombre, token} = datos
